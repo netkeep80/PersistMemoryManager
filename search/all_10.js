fa9dcacabccdb3d460f60b8ed103dd4ce3f3cd2f
netkeep80 <netkeep80@gmail.com>
2026-03-02 20:31:15 +0000

    Deploying to gh-pages from @ netkeep80/PersistMemoryManager@b4dd3b8cef3430dc142944fd5b935dc981895b58 🚀

--- search/all_10.js
+++ search/all_10.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['save_0',['save',['../namespacepmm.html#abfd5a1c07eb8af86044942de369b9c21',1,'pmm']]],
-  ['smallest_5ffree_1',['smallest_free',['../structpmm_1_1MemoryStats.html#a0ef3c7b4b7385778f7e7874f27444433',1,'pmm::MemoryStats']]]
+  ['size_1',['size',['../structpmm_1_1detail_1_1BlockHeader.html#a3aaacb0639cf012b2414cf9b39737c63',1,'pmm::detail::BlockHeader']]],
+  ['smallest_5ffree_2',['smallest_free',['../structpmm_1_1MemoryStats.html#a0ef3c7b4b7385778f7e7874f27444433',1,'pmm::MemoryStats']]]
 ];
