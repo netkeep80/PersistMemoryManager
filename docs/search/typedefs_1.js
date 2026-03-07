640f4615d3023dacbdf7ca1889a5f3f4f1ad52dc
netkeep80 <netkeep80@gmail.com>
2026-03-07 22:46:01 +0000

    Deploying to gh-pages from @ netkeep80/PersistMemoryManager@dfe68bfd3e063b1bb3b2cb1c1d2c5bcab982697d 🚀

--- docs/search/typedefs_1.js
+++ docs/search/typedefs_1.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['base_0',['base',['../classpmm_1_1FreeBlock.html#aace3e9db443dc3d8b367db205e8afffe',1,'pmm::FreeBlock::Base'],['../classpmm_1_1FreeBlockRemovedAVL.html#a948d30da19806a229cd956fd9a5f1b23',1,'pmm::FreeBlockRemovedAVL::Base'],['../classpmm_1_1SplittingBlock.html#ab9e5c55f84ddf334e535a17be64be688',1,'pmm::SplittingBlock::Base'],['../classpmm_1_1AllocatedBlock.html#a071ea70dcde9d1ef914d403bb943df8a',1,'pmm::AllocatedBlock::Base'],['../classpmm_1_1FreeBlockNotInAVL.html#a51fc927035df932b30dd77230c8a7017',1,'pmm::FreeBlockNotInAVL::Base'],['../classpmm_1_1CoalescingBlock.html#ad8294653d10e09a9209f0a94df20b757',1,'pmm::CoalescingBlock::Base']]],
   ['base_5ftype_1',['base_type',['../classpmm_1_1StaticPersistMemoryManager.html#a29f8ecb04dfdb238b6cc0e444c8acc4c',1,'pmm::StaticPersistMemoryManager']]],
-  ['baseblock_2',['BaseBlock',['../classpmm_1_1BlockStateBase.html#a3d02ea578a4b16906ab229108464a909',1,'pmm::BlockStateBase']]]
+  ['baseblock_2',['BaseBlock',['../classpmm_1_1BlockStateBase.html#a3d02ea578a4b16906ab229108464a909',1,'pmm::BlockStateBase']]],
+  ['blockt_3',['blockt',['../classpmm_1_1AllocatorPolicy.html#a1a42eafe9996fd1670f7df34215c8d91',1,'pmm::AllocatorPolicy::BlockT'],['../structpmm_1_1AvlFreeTree.html#a6d65e3deb934ba6864b288c614d3760b',1,'pmm::AvlFreeTree::BlockT']]]
 ];
