ce006fd8108ab98b34b6e6f301eb91627c6076ae
netkeep80 <netkeep80@gmail.com>
2026-03-08 12:54:56 +0000

    Deploying to gh-pages from @ netkeep80/PersistMemoryManager@6acdd172bd67c48c807e7387441e176922072130 🚀

--- docs/search/functions_5.js
+++ docs/search/functions_5.js
@@ -3,6 +3,8 @@ var searchData=
   ['finalize_5fcoalesce_0',['finalize_coalesce',['../classpmm_1_1CoalescingBlock.html#a7ecb2802a98a4dea9891432fe92ec109',1,'pmm::CoalescingBlock']]],
   ['finalize_5fsplit_1',['finalize_split',['../classpmm_1_1SplittingBlock.html#aa6ec0ceea5688a51720b4d1281a55d6a',1,'pmm::SplittingBlock']]],
   ['find_5fbest_5ffit_2',['find_best_fit',['../structpmm_1_1AvlFreeTree.html#a6404447c36b2cb4af2107f99aa7479de',1,'pmm::AvlFreeTree']]],
-  ['free_5fblock_5fcount_3',['free_block_count',['../classpmm_1_1PersistMemoryManager.html#aa83533fb25e93de9f1e42f2ee85d8c65',1,'pmm::PersistMemoryManager']]],
-  ['free_5fsize_4',['free_size',['../classpmm_1_1PersistMemoryManager.html#ac1d0faa6aa26f876603b8badbaeaa3c1',1,'pmm::PersistMemoryManager']]]
+  ['for_5feach_5fblock_3',['for_each_block',['../classpmm_1_1PersistMemoryManager.html#a32b359e75926faeb48211d7019343f09',1,'pmm::PersistMemoryManager']]],
+  ['for_5feach_5ffree_5fblock_4',['for_each_free_block',['../classpmm_1_1PersistMemoryManager.html#aa91cabbb0711fae28590c571dcb1a112',1,'pmm::PersistMemoryManager']]],
+  ['free_5fblock_5fcount_5',['free_block_count',['../classpmm_1_1PersistMemoryManager.html#aa83533fb25e93de9f1e42f2ee85d8c65',1,'pmm::PersistMemoryManager']]],
+  ['free_5fsize_6',['free_size',['../classpmm_1_1PersistMemoryManager.html#ac1d0faa6aa26f876603b8badbaeaa3c1',1,'pmm::PersistMemoryManager']]]
 ];
