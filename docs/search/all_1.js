aaf9021d999db25b9cffa3735f93917fc8baee57
netkeep80 <netkeep80@gmail.com>
2026-03-10 11:19:12 +0000

    Deploying to gh-pages from @ netkeep80/PersistMemoryManager@2b9c4f30b5f3c83ba49922b8a76d08f7466fefef 🚀

--- docs/search/all_1.js
+++ docs/search/all_1.js
@@ -17,13 +17,14 @@ var searchData=
   ['attach_14',['attach',['../classpmm_1_1HeapStorage.html#a61d1dc39749ecf7a1298712f3ae39732',1,'pmm::HeapStorage']]],
   ['avl_5fbalance_5ffactor_15',['avl_balance_factor',['../namespacepmm_1_1detail.html#acebc85a5bef5e325c54cf284da788858',1,'pmm::detail']]],
   ['avl_5fdepth_16',['avl_depth',['../structpmm_1_1FreeBlockView.html#a562fc75f273f514d80eb8cf52d2d732d',1,'pmm::FreeBlockView']]],
-  ['avl_5fheight_17',['avl_height',['../classpmm_1_1BlockStateBase.html#a7a62a48f6d969274d898b7f4575d4aa6',1,'pmm::BlockStateBase::avl_height()'],['../namespacepmm_1_1detail.html#ac7dbac8ee28b2018bd6fad18ec152505',1,'pmm::detail::avl_height()'],['../structpmm_1_1FreeBlockView.html#a16a9cde93981ff6b070813ea7fde9174',1,'pmm::FreeBlockView::avl_height'],['../structpmm_1_1TreeNode.html#aa55953de2301f09d07416860ab05cbf4',1,'pmm::TreeNode::avl_height']]],
-  ['avl_5finsert_18',['avl_insert',['../namespacepmm_1_1detail.html#a72bbba50e064df524cd28744cd06001e',1,'pmm::detail']]],
-  ['avl_5frebalance_5fup_19',['avl_rebalance_up',['../namespacepmm_1_1detail.html#a5b4533a8b630c683666958fa3f9ff20b',1,'pmm::detail']]],
-  ['avl_5frotate_5fleft_20',['avl_rotate_left',['../namespacepmm_1_1detail.html#aaff48ee1921235562c5d71f747e6e3e2',1,'pmm::detail']]],
-  ['avl_5frotate_5fright_21',['avl_rotate_right',['../namespacepmm_1_1detail.html#aaa7038eca67f0b447e63a6026e56f111',1,'pmm::detail']]],
-  ['avl_5fset_5fchild_22',['avl_set_child',['../namespacepmm_1_1detail.html#a6f5e126a98618699c6da582dcf2af1dc',1,'pmm::detail']]],
-  ['avl_5ftree_5fmixin_2eh_23',['avl_tree_mixin.h',['../avl__tree__mixin_8h.html',1,'']]],
-  ['avl_5fupdate_5fheight_24',['avl_update_height',['../namespacepmm_1_1detail.html#ae0ccb7f9648f5db7a8082e2fd5b4506b',1,'pmm::detail']]],
-  ['avlfreetree_25',['avlfreetree',['../structpmm_1_1AvlFreeTree.html#ae47c7c6edaf82e954196c62aaa1bd638',1,'pmm::AvlFreeTree::AvlFreeTree()=delete'],['../structpmm_1_1AvlFreeTree.html#af7bc5f6093df1001d08143e0fe0e0e57',1,'pmm::AvlFreeTree::AvlFreeTree(const AvlFreeTree &amp;)=delete'],['../structpmm_1_1AvlFreeTree.html',1,'pmm::AvlFreeTree&lt; AddressTraitsT &gt;']]]
+  ['avl_5ffind_17',['avl_find',['../namespacepmm_1_1detail.html#a2f10a8cab6d45206d7f6b711354222a4',1,'pmm::detail']]],
+  ['avl_5fheight_18',['avl_height',['../classpmm_1_1BlockStateBase.html#a7a62a48f6d969274d898b7f4575d4aa6',1,'pmm::BlockStateBase::avl_height()'],['../namespacepmm_1_1detail.html#ac7dbac8ee28b2018bd6fad18ec152505',1,'pmm::detail::avl_height()'],['../structpmm_1_1FreeBlockView.html#a16a9cde93981ff6b070813ea7fde9174',1,'pmm::FreeBlockView::avl_height'],['../structpmm_1_1TreeNode.html#aa55953de2301f09d07416860ab05cbf4',1,'pmm::TreeNode::avl_height']]],
+  ['avl_5finsert_19',['avl_insert',['../namespacepmm_1_1detail.html#a72bbba50e064df524cd28744cd06001e',1,'pmm::detail']]],
+  ['avl_5frebalance_5fup_20',['avl_rebalance_up',['../namespacepmm_1_1detail.html#a5b4533a8b630c683666958fa3f9ff20b',1,'pmm::detail']]],
+  ['avl_5frotate_5fleft_21',['avl_rotate_left',['../namespacepmm_1_1detail.html#aaff48ee1921235562c5d71f747e6e3e2',1,'pmm::detail']]],
+  ['avl_5frotate_5fright_22',['avl_rotate_right',['../namespacepmm_1_1detail.html#aaa7038eca67f0b447e63a6026e56f111',1,'pmm::detail']]],
+  ['avl_5fset_5fchild_23',['avl_set_child',['../namespacepmm_1_1detail.html#a6f5e126a98618699c6da582dcf2af1dc',1,'pmm::detail']]],
+  ['avl_5ftree_5fmixin_2eh_24',['avl_tree_mixin.h',['../avl__tree__mixin_8h.html',1,'']]],
+  ['avl_5fupdate_5fheight_25',['avl_update_height',['../namespacepmm_1_1detail.html#ae0ccb7f9648f5db7a8082e2fd5b4506b',1,'pmm::detail']]],
+  ['avlfreetree_26',['avlfreetree',['../structpmm_1_1AvlFreeTree.html#ae47c7c6edaf82e954196c62aaa1bd638',1,'pmm::AvlFreeTree::AvlFreeTree()=delete'],['../structpmm_1_1AvlFreeTree.html#af7bc5f6093df1001d08143e0fe0e0e57',1,'pmm::AvlFreeTree::AvlFreeTree(const AvlFreeTree &amp;)=delete'],['../structpmm_1_1AvlFreeTree.html',1,'pmm::AvlFreeTree&lt; AddressTraitsT &gt;']]]
 ];
