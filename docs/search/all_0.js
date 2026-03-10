41f7fa5f492285748b649936231665744924f3be
netkeep80 <netkeep80@gmail.com>
2026-03-10 18:01:57 +0000

    Deploying to gh-pages from @ netkeep80/PersistMemoryManager@fea6c385e150e3c65569ebe01aa9c55d5eabad3a 🚀

--- docs/search/all_0.js
+++ docs/search/all_0.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['_5froot_5fidx_0',['_root_idx',['../structpmm_1_1pmap.html#a47c56cc56749ed033d17b398dbe86cfb',1,'pmm::pmap::_root_idx'],['../structpmm_1_1pstringview.html#a1875b7f238768665cb881b991bd28c62',1,'pmm::pstringview::_root_idx']]]
+  ['_5fpad_0',['_pad',['../structpmm_1_1detail_1_1ManagerHeader.html#a94c052d285a101c0fe3e26d9f0d8019e',1,'pmm::detail::ManagerHeader']]],
+  ['_5freserved_1',['_reserved',['../structpmm_1_1detail_1_1ManagerHeader.html#a95b7c2bdb1d0e22258ed0e07e96e4898',1,'pmm::detail::ManagerHeader']]],
+  ['_5froot_5fidx_2',['_root_idx',['../structpmm_1_1pmap.html#a47c56cc56749ed033d17b398dbe86cfb',1,'pmm::pmap::_root_idx'],['../structpmm_1_1pstringview.html#a1875b7f238768665cb881b991bd28c62',1,'pmm::pstringview::_root_idx']]]
 ];
