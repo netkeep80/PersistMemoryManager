f5b751de6333bf84039d66d874329a6093d56727
netkeep80 <netkeep80@gmail.com>
2026-03-11 18:55:10 +0000

    Deploying to gh-pages from @ netkeep80/PersistMemoryManager@8784b4a4d60f5f73770546b44d08aa38a68c7f6e 🚀

--- docs/search/typedefs_c.js
+++ docs/search/typedefs_c.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['thread_5fpolicy_0',['thread_policy',['../classpmm_1_1PersistMemoryManager.html#ad3666dc713110ae501fdb75922b6357b',1,'pmm::PersistMemoryManager']]],
-  ['tinyaddresstraits_1',['TinyAddressTraits',['../namespacepmm.html#abb45c05352727cde1c45f1d73c0dad3b',1,'pmm']]]
+  ['thread_5fpolicy_0',['thread_policy',['../classpmm_1_1PersistMemoryManager.html#ad3666dc713110ae501fdb75922b6357b',1,'pmm::PersistMemoryManager']]]
 ];
