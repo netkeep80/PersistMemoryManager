2d1b8cd90590ee3c2b1e5b7cd0bb9524678dab14
netkeep80 <netkeep80@gmail.com>
2026-04-11 07:09:14 +0000

    Deploying to gh-pages from @ netkeep80/PersistMemoryManager@2ef8f2979b1186f9174d9551a24a22065e215230 🚀

--- docs/search/all_14.js
+++ docs/search/all_14.js
@@ -1,7 +1,8 @@
 var searchData=
 [
-  ['value_0',['value',['../structpmm_1_1pmap__node.html#a9bf0fa400dab525be4256ad650bd975b',1,'pmm::pmap_node']]],
-  ['value_5ftype_1',['value_type',['../structpmm_1_1pallocator.html#af6f82d3a88c4d648e04a0b238eb5d37c',1,'pmm::pallocator::value_type'],['../structpmm_1_1parray.html#a92ed4eb5375a3585dea8599cfc98d941',1,'pmm::parray::value_type'],['../structpmm_1_1ppool.html#aa5fedb7b549f666802d4e4c44966e879',1,'pmm::ppool::value_type']]],
-  ['verify_5finvariants_2',['verify_invariants',['../classpmm_1_1FreeBlock.html#aa42f1282e63621c7b8795f1175ea4ade',1,'pmm::FreeBlock::verify_invariants()'],['../classpmm_1_1AllocatedBlock.html#a9eba97f1e5e4221a632689ac3b6100f1',1,'pmm::AllocatedBlock::verify_invariants()']]],
-  ['version_3',['version',['../structpmm_1_1detail_1_1ForestDomainRegistry.html#afe02953f24d7c967b69c2a0f0aeb8705',1,'pmm::detail::ForestDomainRegistry']]]
+  ['validate_5fbootstrap_5finvariants_0',['validate_bootstrap_invariants',['../classpmm_1_1PersistMemoryManager.html#ab039de38337c947652bffb167ca04b67',1,'pmm::PersistMemoryManager']]],
+  ['value_1',['value',['../structpmm_1_1pmap__node.html#a9bf0fa400dab525be4256ad650bd975b',1,'pmm::pmap_node']]],
+  ['value_5ftype_2',['value_type',['../structpmm_1_1pallocator.html#af6f82d3a88c4d648e04a0b238eb5d37c',1,'pmm::pallocator::value_type'],['../structpmm_1_1parray.html#a92ed4eb5375a3585dea8599cfc98d941',1,'pmm::parray::value_type'],['../structpmm_1_1ppool.html#aa5fedb7b549f666802d4e4c44966e879',1,'pmm::ppool::value_type']]],
+  ['verify_5finvariants_3',['verify_invariants',['../classpmm_1_1FreeBlock.html#aa42f1282e63621c7b8795f1175ea4ade',1,'pmm::FreeBlock::verify_invariants()'],['../classpmm_1_1AllocatedBlock.html#a9eba97f1e5e4221a632689ac3b6100f1',1,'pmm::AllocatedBlock::verify_invariants()']]],
+  ['version_4',['version',['../structpmm_1_1detail_1_1ForestDomainRegistry.html#afe02953f24d7c967b69c2a0f0aeb8705',1,'pmm::detail::ForestDomainRegistry']]]
 ];
--- docs/search/functions_12.js
+++ docs/search/functions_12.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['verify_5finvariants_0',['verify_invariants',['../classpmm_1_1FreeBlock.html#aa42f1282e63621c7b8795f1175ea4ade',1,'pmm::FreeBlock::verify_invariants()'],['../classpmm_1_1AllocatedBlock.html#a9eba97f1e5e4221a632689ac3b6100f1',1,'pmm::AllocatedBlock::verify_invariants()']]]
+  ['validate_5fbootstrap_5finvariants_0',['validate_bootstrap_invariants',['../classpmm_1_1PersistMemoryManager.html#ab039de38337c947652bffb167ca04b67',1,'pmm::PersistMemoryManager']]],
+  ['verify_5finvariants_1',['verify_invariants',['../classpmm_1_1FreeBlock.html#aa42f1282e63621c7b8795f1175ea4ade',1,'pmm::FreeBlock::verify_invariants()'],['../classpmm_1_1AllocatedBlock.html#a9eba97f1e5e4221a632689ac3b6100f1',1,'pmm::AllocatedBlock::verify_invariants()']]]
 ];
