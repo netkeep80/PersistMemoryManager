d7ca5d43549f70b1e4210d8d16d75ad33149814a
konard <drakonard@gmail.com>
2026-04-20 15:18:24 +0000

    style: satisfy ci clang-format

--- single_include/pmm/pmm_no_comments.h
+++ single_include/pmm/pmm_no_comments.h
@@ -3395,7 +3395,7 @@ template <typename ManagerAccess> struct ManagerLayoutOps
         std::size_t min_need = static_cast<std::size_t>( ManagerAccess::kBlockHdrGranules + data_gran_need +
                                                          ManagerAccess::kBlockHdrGranules ) *
                                kGranSz;
-        std::size_t growth   = old_size / 4;
+        std::size_t growth = old_size / 4;
         if ( growth < min_need )
             growth = min_need;
 
